3075a963bd9d2f8f9a1f02b598f5c5eb76cbdd78
Andrey Tselishchev <vorteq@gmail.com>
2022-09-15 21:31:43 +0300

    Add notes, working on image editing

---
+++ color_clash/NOTES.md
@@ -0,0 +1,13 @@
+
+Tools:
+ - Brush. Pattern: dot, etc..
+ - Lines, polylines
+ - Rectangle/square
+ - Circle/ellipse
+ - Fill: solid/texture
+ - Select/copy/paste (snap to blocks)
+ - Zoom 2x 4x 8x
+
+ - Color palette
+ - Mode: xor, or, and
+ - Attr: nop
--- color_clash/src/widgets/frame_buffer_view.rs
+++ color_clash/src/widgets/frame_buffer_view.rs
@@ -1,6 +1,8 @@
 use druid::{
-    piet::{ImageFormat, InterpolationMode},
-    widget::prelude::*
+    piet::{ImageFormat, InterpolationMode, StrokeStyle},
+    kurbo::{Line, Rect},
+    widget::prelude::*,
+    Event, MouseButton, Point, Color,
 };
 
 use crate::models::FrameBuffer;
@@ -14,20 +16,41 @@ pub struct FrameBufferView {
 impl FrameBufferView {
 
     pub fn new() -> Self {
-        Self { base_zoom: 4.0, zoom: 1.0, image: vec!() }
+        Self { base_zoom: 2.0, zoom: 1.0, image: vec!() }
+    }
+
+    fn effective_zoom(&self) -> f64 {
+        self.base_zoom * self.zoom
+    }
+
+    fn widget2fb_coords(&self, point: Point) -> (usize, usize) {
+        (
+            (point.x / self.effective_zoom()) as usize,
+            (point.y / self.effective_zoom()) as usize
+        )
     }
 
 }
 
 impl Widget<FrameBuffer> for FrameBufferView {
 
-    fn event(&mut self, _ctx: &mut EventCtx, _event: &Event, _data: &mut FrameBuffer, _env: &Env) {}
+    fn event(&mut self, _ctx: &mut EventCtx, event: &Event, data: &mut FrameBuffer, _env: &Env) {
+        match event {
+            Event::MouseDown(e) if e.button == MouseButton::Left => {
+                let (x, y) = self.widget2fb_coords(e.pos);
+                data.pixels[data.width * y + x] = true;
+            },
+            Event::MouseMove(e) => {},
+            _ => {}
+        }
+    }
 
     fn lifecycle(&mut self, _ctx: &mut LifeCycleCtx, _event: &LifeCycle, _data: &FrameBuffer, _env: &Env) {}
 
-    fn update(&mut self, _ctx: &mut UpdateCtx, old_data: &FrameBuffer, data: &FrameBuffer, _env: &Env) {
+    fn update(&mut self, ctx: &mut UpdateCtx, old_data: &FrameBuffer, data: &FrameBuffer, _env: &Env) {
         if !data.same(old_data) {
             self.image = data.to_rgb_image();
+            ctx.request_paint();
         }
     }
 
@@ -39,12 +62,23 @@ impl Widget<FrameBuffer> for FrameBufferView {
     }
 
     fn paint(&mut self, ctx: &mut PaintCtx, data: &FrameBuffer, _env: &Env) {
+
         if self.image.is_empty() {
             self.image = data.to_rgb_image();
         }
-        let rect = ctx.size().to_rect();
+
+        // Draw image
+        let image_size = ctx.size();
         let image = ctx.make_image(data.width, data.height, &self.image, ImageFormat::Rgb).unwrap();
-        ctx.draw_image(&image, rect, InterpolationMode::NearestNeighbor);
+        ctx.draw_image(&image, image_size.to_rect(), InterpolationMode::NearestNeighbor);
+
+        // Draw attribute grid
+        let stroke_style = StrokeStyle::new().dash_pattern(&[4.0, 4.0]);
+        ctx.stroke_styled(
+            Rect::from_origin_size(Point::ORIGIN, image_size),
+            &Color::YELLOW, 1.0, &stroke_style
+        );
+
     }
 
 }
